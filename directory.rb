79683815add3cb1278de5f0cc654bdfe0819d200
Tayanne Bencardino <tayannebencardino@gmail.com>
2022-06-05 15:35:53 +0100

    Refactoring

--- directory.rb
+++ directory.rb
@@ -5,13 +5,21 @@ students = [
 "Terminator", "Freddy Krueger", "The Joker",
 "Joffrey Baratheon", "Norman Bates"    
 ]
-# and then print them
-puts "The students of Villains Academy"
-puts "-------------"
-students.each do |student|
-    puts student
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
 end
 
-# finally, we print the total number of students
-puts "Overall, we have #{students.count} great students"
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
 
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+print_header
+print(students)
+print_footer(students)
