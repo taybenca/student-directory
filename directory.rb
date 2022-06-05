779efcc7007816b4cae887bb145951ff144b7793
Tayanne Bencardino <tayannebencardino@gmail.com>
2022-06-05 22:07:49 +0100

    Add cohort input

--- directory.rb
+++ directory.rb
@@ -1,17 +1,23 @@
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts "Please enter the names of the student and hit return, the cohort and hit return"
+  puts "To finish, just hit return three times"
   # create and empty array 
   students = []
   # get the first name
   name = gets.chomp
+  cohort = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    students << {name: name, cohort: cohort, hobbies: :empty, country_of_birth: :empty, height: :empty}
+    if students.count == 1
+      puts "Now we have 1 student"
+    else
+      puts "Now we have #{students.count} students"
+    end
     # get another name from the user
     name = gets.chomp
+    cohort = gets.chomp
   end
   # return the array of students
   students
@@ -24,9 +30,7 @@ end
 
 def print(students)
   students.each_with_index do |student, index|
-    if student[:name].length < 12
       puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
-    end
   end
 end
   
