a6bd06538cdcdaaa88b1c543c2bb2e2b9b6ca4e8
Tayanne Bencardino <tayannebencardino@gmail.com>
2022-06-05 15:57:07 +0100

    Refactoring with hashes

--- directory.rb
+++ directory.rb
@@ -1,18 +1,25 @@
 # let's put all student into an array
 students = [
-"Dr. Hannibal Lecter", "Darth Vader", "Nurse Ratched",
-"Michael Corleone", "Alex DeLarge", "The Wicked Witch of the West",
-"Terminator", "Freddy Krueger", "The Joker",
-"Joffrey Baratheon", "Norman Bates"    
+  {name: "Dr. Hannibal Lecter", cohort: :november},
+  {name: "Darth Vader", cohort: :november},
+  {name: "Nurse Ratched", cohort: :november},
+  {name: "Michael Corleone", cohort: :november},
+  {name: "Alex DeLarge", cohort: :november},
+  {name: "The Wicked Witch of the West", cohort: :november},
+  {name: "Terminator", cohort: :november},
+  {name: "Freddy Krueger", cohort: :november},
+  {name: "The Joker", cohort: :november},
+  {name: "Joffrey Baratheon", cohort: :november}, 
+  {name: "Norman Bates", cohort: :november}
 ]
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
 
-def print(names)
-  names.each do |name|
-    puts name
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
