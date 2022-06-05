c4204442154c5254250af497451344ece82c421f
Tayanne Bencardino <tayannebencardino@gmail.com>
2022-06-05 17:46:33 +0100

    Print only if is more than 12 characters

--- directory.rb
+++ directory.rb
@@ -24,7 +24,9 @@ end
 
 def print(students)
   students.each_with_index do |student, index|
-    puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    if student[:name].length < 12
+      puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    end
   end
 end
   
