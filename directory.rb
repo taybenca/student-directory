8c5713d8690ac15569cbdc93456ad8f7f4b6d2db
Tayanne Bencardino <tayannebencardino@gmail.com>
2022-06-07 15:01:37 +0100

    Refactoring the code

--- directory.rb
+++ directory.rb
@@ -1,26 +1,50 @@
+@students = []
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def interactive_menu
+  loop do
+    print_menu()
+    process(gets.chomp)
+  end
+end
+
+def process(selection)
+  case selection
+    when "1"
+      @students = input_students()
+    when "2"
+      show_students()
+    when "9"
+      exit
+    else 
+      puts "I don't know what you meant, try again"
+    end
+end
+
 def input_students
-  puts "Please enter the names of the student and hit return, the cohort and hit return"
-  puts "To finish, just hit return three times"
-  # create and empty array 
-  students = []
-  # get the first name
+  puts "Please enter the names of the student and hit return."
+  puts "To finish, just hit return twice"
   name = gets.chomp
-  cohort = gets.chomp
-  # while the name is not empty, repeat this code
   while !name.empty? do
-    # add the student hash to the array
-    students << {name: name, cohort: cohort, hobbies: :empty, country_of_birth: :empty, height: :empty}
-    if students.count == 1
+    @students << {name: name, cohort: :november, hobbies: :empty, country_of_birth: :empty, height: :empty}
+    if @students.count == 1
       puts "Now we have 1 student"
     else
-      puts "Now we have #{students.count} students"
+      puts "Now we have #{@students.count} students"
     end
-    # get another name from the user
-    name = gets.chop
-    cohort = gets.chop
+    name = gets.chomp
   end
-  # return the array of students
-  students
+  @students
+end
+
+def show_students
+  print_header()
+  print_students_list(@students)
+  print_footer(@students)
 end
 
 def print_header
@@ -28,10 +52,10 @@ def print_header
     puts "-------------"
 end
 
-def print(students)
-  if students.length >=1
-    students.each_with_index do |student, index|
-      puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)".center(50)
+def print_students_list(students)
+  if @students.length >=1
+    @students.each_with_index do |student, index|
+      puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"#.center(50)
     end
   end
 end
@@ -40,7 +64,5 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+interactive_menu
+
