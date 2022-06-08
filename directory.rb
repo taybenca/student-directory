c9abd1170b300a72fd1ebb0e511f732d6ba76001
Tayanne Bencardino <tayanne@Tayannes-MacBook-Air.local>
2022-06-08 12:46:05 +0100

    We can save the list in another file and ignore it

--- directory.rb
+++ directory.rb
@@ -2,6 +2,7 @@
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -18,6 +19,8 @@ def process(selection)
       @students = input_students()
     when "2"
       show_students()
+    when "3"
+      save_students()
     when "9"
       exit
     else 
@@ -45,6 +48,18 @@ def show_students
   print_header()
   print_students_list(@students)
   print_footer(@students)
+end
+
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 def print_header
