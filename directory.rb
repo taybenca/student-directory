d33e3bc95751d73419f82fd826915db92c2e6587
Tayanne Bencardino <tayanne@Tayannes-MacBook-Air.local>
2022-06-08 16:17:29 +0100

    Loading students list

--- directory.rb
+++ directory.rb
@@ -3,6 +3,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv./"
   puts "9. Exit"
 end
 
@@ -21,6 +22,8 @@ def process(selection)
       show_students()
     when "3"
       save_students()
+    when "4"
+      load_students()
     when "9"
       exit
     else 
@@ -60,6 +63,16 @@ def save_students
     file.puts csv_line
   end
   file.close
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+  puts @students
 end
 
 def print_header
